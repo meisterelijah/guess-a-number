98593cfb3d38b05f55a0ee39fe295c80d710028a
LeBlanc <elebla8624@greenville.k12.sc.us>
2017-09-22 14:11:14 -0400

    improve the changes to guess a number

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,11 +1,11 @@
 
         
 import random
-
+import math
 # config
 low = 1
-high = 10
-limit = 4
+high = 100
+limit = math.ceil(math.log(100,2))
 
 # helper functions
 def show_start_screen():
@@ -14,7 +14,8 @@ def show_start_screen():
     print("**************************")
 
 def show_credits():
-    print("This dope game was created by.... YOUR MOM!!!!.")
+    print("********************This dope game was created by***************")
+    print("***************************** THE AMAZING ELI!!!!*********************")
     
 def get_guess():
     while True:
@@ -25,27 +26,38 @@ def get_guess():
             return guess
         else:
             print("You must enter a number.")
+            print()
+            
 
 def pick_number():
-    print("I'm thinking of a number from " + str(low) + " to " + str(high) +".")
+    print("I'm thinking of a number from " + str(low) + " to " + str(high) +". you have" +  str (limit) + " tries.")
+    print()
+
 
     return random.randint(low, high)
 
 def check_guess(guess, rand):
     if guess < rand:
         print("You guessed too low.")
+        print()
     elif guess > rand:
         print("You guessed too high.")
+        print()
 
 def show_result(guess, rand):
     if guess == rand:
         print("You win! Your so good at this now move out of your moms basement and go get a life.")
+        print()
+
     else:
         print("you are so stupid! you should be deleted out of space and time. The number was " + str(rand) + ".")
+        print()
 
 def play_again():
     while True:
         decision = input("Would you like to play again? (y/n) ")
+        decision = decision.lower()
+        
 
         if decision == 'y' or decision == 'yes':
             return True
